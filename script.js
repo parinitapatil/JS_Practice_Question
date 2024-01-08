b986811f80f7464dd921c96efcec67318b127e37
parinita patil <parinitapatil805@gmail.com>
2024-01-08 11:02:20 +0530

    second commit

--- script.js
+++ script.js
@@ -45,3 +45,74 @@
 //     sum += i;
 // }
 // console.log(sum)
+
+// Coding challenge #8: Create a function that will convert from Celsius to Fahrenheit✅
+// function celToFer(Temperatue) {
+
+//     // formula: °F = °C × (9/5) + 32
+//     return((Temperatue * (9/5) + 32))
+// }
+// console.log(celToFer(20)+" °F");
+
+// Coding challenge #9: Create a function that will convert from Fahrenheit to Celsius✅
+// function ferTocel(Temperatue) {
+//     // Formula: C = 5/9(F-32)
+//     return(5/9 * (Temperatue-32))
+// }
+// console.log(ferTocel(98.6) + " °C");
+
+// Coding challenge #10: Calculate the sum of numbers in an array of numbers✅
+// function sumOfarray(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++)
+//         sum = sum + arr[i];
+//     return(sum)
+// }
+// let arr = [2, 3, -1, 5, 7, 9, 10, 15, 95];
+// console.log(sumOfarray(arr))
+
+// Coding challenge #11: Calculate the average of the numbers in an array of numbers✅
+// function avgOfarray(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++)
+//         sum += arr[i];
+//     let Avg = sum / arr.length
+//     return(Avg)
+// }
+
+// let arr = [1, 3, 9, 15, 90];
+// console.log(avgOfarray(arr))
+
+// Coding challenge #12: Create a function that receives an array of numbers and returns an array containing only the positive numbers✅
+// function arrOfPositiveNum(arr) {
+//     let newArray = [];
+//     for (let i = 0; i < arr.length; i++)
+//         if (arr[i] >= 0) {
+//             newArray.push(arr[i]);
+//         }
+//     return newArray
+// }
+
+// let arr = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+// console.log(arrOfPositiveNum(arr))
+
+// Coding challenge #13: Find the maximum number in an array of numbers✅
+// function maxNumInArray(arr) {
+//     let sortArray = arr.sort();
+//     sortArray[sortArray.length-1]
+//     return sortArray[sortArray.length - 1];
+    
+// }
+// let arr = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+// console.log(maxNumInArray(arr))
+
+// Coding challenge #14: Print the first 10 Fibonacci numbers without recursion✅
+//     let f0 = 0;
+//     let f1 = 1;
+// for (let i = 0; i <= 10; i++) {
+//         console.log(f0);
+//         let fi = f0 + f1;
+
+//         f0 = f1;
+//         f1 = fi;
+//     }
