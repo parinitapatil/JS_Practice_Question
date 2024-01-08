54f8ba9b10225a5a8f9fb4704de908474568f141
parinita patil <parinitapatil805@gmail.com>
2024-01-08 09:23:28 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// //  #1 : Print numbers from 1 ro 10 ✅
+// for (let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+// //  Coding challenge #2: Print the odd numbers less than 100 ✅
+// // num%2==0    even  , else odd
+
+// for (let i = 0; i <= 100; i++){
+//     if(i%2!=0) console.log(i)
+// }
+
+// // Coding challenge #3: Print the multiplication table with 7✅
+// // num * i = peoduct
+// let num = 7;
+// for (let i = 1; i <= 10; i++){
+//     console.log(num+" x "+ i +" = "+num*i)
+// }
+
+// Coding challenge #4: Print all the multiplication tables with numbers from 1 to 10✅
+// for (let i = 1; i <= 10; i++){
+//     for (let j = 1; j <= 10; j++)
+//     console.log(`${i}x${j}=${i*j}`)
+// }
+
+// Coding challenge #5: Calculate the sum of numbers from 1 to 10✅
+// let sum = 0;
+// for (let i = 1; i <= 10; i++){
+//     console.log(sum+=i)
+// }
+
+// Coding challenge #6: Calculate 10!✅
+// let init = 1;
+// for (let i = 1; i <= 10; i++){
+//     init*=i
+
+// }
+// console.log(init);
+
+// Coding challenge #7: Calculate the sum of odd numbers greater than 10 and less than 30✅
+// sum=0 > if n%2!=0
+// let sum = 0;
+// for (let i = 11; i <= 30; i++){
+//     if (i % 2 != 0)
+//     sum += i;
+// }
+// console.log(sum)
